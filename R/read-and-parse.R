6571fcedd0e9965475be0b6297ca51b4ef2f4b4c
kota <kmori05@gmail.com>
2016-01-26 01:35:01 +0900

    basic parser

--- R/read-and-parse.R
+++ R/read-and-parse.R
@@ -3,21 +3,41 @@
 #'
 #' @name readSGF
 #' @param path    A character representing the location of a smart go format (SGF) file.
-#' @param fileEncoding  A character string: if non-empty declares the encoding used on a file.
+#' @param encoding  A character string: if non-empty declares the encoding used on a file.
 #' @export
-readSGF <- function(path, fileEncoding = "") {
-  scan(path, what = "character", sep = "\n",
-       quiet = TRUE,fileEncoding = fileEncoding) %>%
-    paste(collapse = "") %>%
+readSGF <- function(path, encoding = "") {
+  readLines(path, encoding = encoding) %>%
+    paste0(collapse = "\n") %>%
     parseSGF()
 }
 
 
 
-#' Parse SGF file(s)
+#' Parse text in the smart go format
 #'
 #' @name parseSGF
 #' @param sgf     A character vector of sgf text.
 #' @export
 parseSGF <- function(sgf) {
+  ### pruning ###
+  # TODO: branch pruning
+
+
+  ### obtain meta information ###
+  metatag <- c("PW", "PB", "WR", "BR", "SZ", "KM", "HA",
+               "DT", "RU", "EV", "RO")
+  metainfo <- stringr::str_match(
+    sgf, paste(metatag, "\\[(.*?)\\]", sep = ""))[, 2]
+  names(metainfo) <- metatag
+
+
+  ### parse moves ###
+  # TODO: (handicap tags, AB & AW)
+  tmp <- stringr::str_match_all(
+    sgf, ";([BW])\\[(.*?)\\]")[[1]][, -1]
+  moves <- cbind(
+    sapply(tmp[, 2], utf8ToInt) %>% t() - 96,
+    ifelse (tmp[, 1]=="B", BLACK, WHITE))
+  colnames(moves) <- c("x", "y", "col")
+  sgf
 }
--- R/sgf.R
+++ R/sgf.R
@@ -1,5 +1,9 @@
+### define global constants ###
+BLACK <- 1
+WHITE <- 2
+BLANK <- 0
 
 
 
+### manage imports ###
 #' @importFrom magrittr %>%
-NULL
