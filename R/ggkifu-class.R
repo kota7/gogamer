7c8b37137038ba54b07a25f63b332f269a1f503a
kota <kmori05@gmail.com>
2016-07-11 02:05:56 +0900

    renamed file

--- R/ggkifu-class.R
+++ R/ggkifu-class.R
@@ -1,15 +1,15 @@
 
-#' Constructor of kifuplot object
+#' One-page kifu image
 #' @param board \code{ggplot} object of board
 #' @param note \code{ggplot} object of outside note
 #' @param boardsize integer of board size
 #' @param notenrow integer of number of rows of note
-#' @return \code{kifuimage} object
-kifuimage <- function(board, note, boardsize, notenrow)
+#' @return \code{ggkifu} object
+ggkifu <- function(board, note, boardsize, notenrow)
 {
   return(structure(.Date = list(
     board = board, note = note, boardsize = boardsize, notenrow = notenrow),
-    class = "kifuimage"))
+    class = "ggkifu"))
 }
 
 
