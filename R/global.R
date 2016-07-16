cad3bc0212fe0e7ebcfe9fa2ea55a038677b41ad
kota <kmori05@gmail.com>
2016-07-17 03:54:54 +0900

    slight color change

--- R/global.R
+++ R/global.R
@@ -25,23 +25,23 @@ WHITE <- 2L
 
 
     # stone
-    blackcolor = "#000000", whitecolor = "#ffffff", stonelinecolor = "#000000",
+    blackcolor = "#111111", whitecolor = "#f5f5f5", stonelinecolor = "#101010",
     stonesize = 6, stonelinewidth = 0.7,
 
     # numbers
-    whitenumbercolor = "#000000", blacknumbercolor = "#ffffff",
+    whitenumbercolor = "#0f0f0f", blacknumbercolor = "#f0f0f0",
     numbersize = 3,
 
     # label
-    whitelabelcolor = "#000000",
-    blacklabelcolor = "#ffffff",
+    whitelabelcolor = "#0e0e0e",
+    blacklabelcolor = "#f0f0f0",
     emptylabelcolor = "#262626",
     emptylabelshadowsize = 5,       # size of shadow around empty markers
     labelsize = 3.5,
 
     # marker
-    whitemarkercolor = "#000000",
-    blackmarkercolor = "#ffffff",
+    whitemarkercolor = "#0e0e0e",
+    blackmarkercolor = "#f0f0f0",
     emptymarkercolor = "#262626",  # empty markers are markers not on stone
     markersize = 2.5,
 
@@ -50,18 +50,18 @@ WHITE <- 2L
     # score points
     territorysize = 2,
     territoryshape = 21,
-    territorylinecolor = "#000000",
+    territorylinecolor = "#262626",
     territoryslinewidth = 0.5,
 
     # board
-    boardcolor = "#e1f0c0", gridcolor  = "#262626",
+    boardcolor = "#e2f1c1", gridcolor  = "#262626",
     axislabelcolor = "#262626", axislabelsize = 3.5,
     starcolor = "#262626", starsize = 1.5,
     # axis labels
     xlabels = LETTERS[-9], ylabels = as.character(1:25),
 
     # kifu outside notes
-    notebackcolor = "#d2f1f1", notetextcolor = "#262626",
+    notebackcolor = "#eff7df", notetextcolor = "#101010",
     notestonesize = 4.5, notenumbersize = 2.5,
     notetextsize = 3,
     moveperrow = 8,  # number of moves to show per line in the outside note
